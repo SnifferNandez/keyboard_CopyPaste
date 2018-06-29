773680ea426552e013862b92a08909b0113a85b1
SnifferNandez <C4Fdez+31337@gmail.com>
2018-06-29 09:51:07 -0500

    Basic functions

--- kcp.py
+++ kcp.py
@@ -5,11 +5,18 @@ import xerox
 
 useIfStarts = 'keypaste'
 
+def PressKey(KeyUnicode):
+  print KeyUnicode
+
+def PasteKeystrokes(clipboard):
+  for k in clipboard:
+    PressKey(k)
+    #PressKey(unichr(0x12)) #Alt
+
 recent_value = ""
 while True:
   tmp_value = xerox.paste()
   if useIfStarts == tmp_value[0:len(useIfStarts)] and tmp_value != recent_value:
     recent_value = tmp_value
-    print "Value changed: %s" % str(recent_value)
-    #UnicodeEncodeError: 'ascii' codec can't encode character u'\xb7' in position 42: ordinal not in range(128)
+    PasteKeystrokes(tmp_value[len(useIfStarts):])
   time.sleep(0.1)
